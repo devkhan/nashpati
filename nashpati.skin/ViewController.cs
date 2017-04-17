e624706227982e3bd53889b1818bfdee0afd7f32
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2017-04-17 15:03:52 +0530

    Initial commit: setup project.

---
+++ nashpati.skin/ViewController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace nashpati.skin
+{
+	public partial class ViewController : NSViewController
+	{
+		private int numberOfTimesClicked = 0;
+
+		public ViewController(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			// Do any additional setup after loading the view.
+			ClickedLabel.StringValue = "Button has not been clicked yet.";
+		}
+
+		public override NSObject RepresentedObject
+		{
+			get
+			{
+				return base.RepresentedObject;
+			}
+			set
+			{
+				base.RepresentedObject = value;
+				// Update the view, if already loaded.
+			}
+		}
+
+		partial void ClickedButton(AppKit.NSButton sender)
+		{
+
+			// Update counter and label
+			ClickedLabel.StringValue = string.Format("The button has been clicked {0} time{1}.", ++numberOfTimesClicked, (numberOfTimesClicked < 2) ? "" : "s");
+		}
+	}
+}
